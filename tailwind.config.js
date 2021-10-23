4b6a41fe7d57e06f9ed0911b17748702e12f85a7
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2021-10-23 16:14:14 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary:{
+          light:'#3498db',
+          dark:'#2980b9',
+        },
+        secondary:'#e67e22',
+      },
+      fontFamily:{
+        "head":['"Dancing Script"'],
+        "title":['Montserrat'],
+      }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
